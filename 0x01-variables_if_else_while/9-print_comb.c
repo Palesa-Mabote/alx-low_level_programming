c83835ed05a2a2728fc046e28945bf4ffb90db38
Palesa-Mabote <verochmabote@gmail.com>
2023-03-05 07:38:54 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - main function
- *
- * Return: always o
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (n = 48; n < 58; n++)
 	{
-		        putchar(i + '0');
-		 if (i != 9)
-		 {
-		       putchar(',');
-	               putchar(' ');
-	         }
-                 putchar('\n');
-                 return (0);
-        }
-}	
-	  
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
